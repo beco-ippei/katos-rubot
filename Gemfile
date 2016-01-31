a096594984fa695ea9128c406dc4e328aed92aa2
beco-ippei <beco.ippei@gmail.com>
2016-02-01 00:31:01 +0900

    init

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+#gem "slack-bot-slim", path: '/var/devel/slack-bot-slim'
+gem "slack-bot-slim", git: 'https://github.com/beco-ippei/slack-bot-slim'
+
---
+++ bot.rb
@@ -0,0 +1,12 @@
+require 'slack_bot_slim'
+
+SlackBot.token = ENV['token']
+bot = SlackBot.instance
+
+$LOAD_PATH.unshift File.expand_path('../app', __FILE__)
+
+require 'application'
+
+# start
+bot.start
+
